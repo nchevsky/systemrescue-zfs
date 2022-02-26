91b0ce1cb6dc1501af9bf55eb5200cab71210559
Francois Dupoux <2386566-fdupoux@users.noreply.gitlab.com>
2022-02-26 17:38:12 +0000

    Support for building the i686 edition in docker

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Parameters
-architecture="x86_64"
-dockerimg="sysrescuebuildiso-${architecture}:latest"
+# Parameters validation
+if [ -z "${sysrescuearch}" ] ; then
+    echo "ERROR: You must define the environment variable 'sysrescuearch' as either 'x86_64' or 'i686' before you run this script"
+    exit 1
+fi
+
+if [ "${sysrescuearch}" != "x86_64" ] && [ "${sysrescuearch}" != "i686" ] ; then
+    echo "Value '${sysrescuearch}' is invalid for environment variable 'sysrescuearch'. Only 'x86_64' and 'i686' are supported"
+    exit 1
+fi
 
 # Determine the path to the git repository
 fullpath="$(realpath $0)"
@@ -17,7 +24,8 @@ mkdir -p ${tmpdir}
 cp -a ${repodir}/pacman.conf ${tmpdir}
 
 # Build the docker image
-docker build -t ${dockerimg} -f ${repodir}/docker/Dockerfile-build-iso-${architecture} ${repodir}/docker
+dockerimg="sysrescuebuildiso-${sysrescuearch}:latest"
+docker build -t ${dockerimg} -f ${repodir}/docker/Dockerfile-build-iso-${sysrescuearch} ${repodir}/docker
 
 # Cleanup
 rm -rf ${tmpdir}
--- docker/build-iso-image.sh
+++ docker/build-iso-image.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Parameters
-architecture="x86_64"
-dockerimg="sysrescuebuildiso-${architecture}:latest"
+# Parameters validation
+if [ -z "${sysrescuearch}" ] ; then
+    echo "ERROR: You must define the environment variable 'sysrescuearch' as either 'x86_64' or 'i686' before you run this script"
+    exit 1
+fi
+
+if [ "${sysrescuearch}" != "x86_64" ] && [ "${sysrescuearch}" != "i686" ] ; then
+    echo "Value '${sysrescuearch}' is invalid for environment variable 'sysrescuearch'. Only 'x86_64' and 'i686' are supported"
+    exit 1
+fi
 
 # Make sure the docker image exists
+dockerimg="sysrescuebuildiso-${sysrescuearch}:latest"
 if ! docker inspect ${dockerimg} >/dev/null 2>/dev/null ; then
     echo "ERROR: You must build the following docker image before you run this script: ${dockerimg}"
     exit 1
@@ -30,4 +38,4 @@ fi
 docker run --user 0:0 --privileged -it --workdir /workspace \
     --volume=${repodir}:/workspace \
     --volume=${pkgcache}:/var/cache/pacman/pkg \
-    ${dockerimg} setarch ${architecture} /bin/bash -x /workspace/build.sh "$@"
+    ${dockerimg} setarch ${sysrescuearch} /bin/bash /workspace/build.sh "$@"
